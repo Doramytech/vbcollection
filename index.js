18be57719a119482c43b33cad1dc34691b5d4370
Doramytech <pelumisobaloju@gmail.com>
2026-03-28 03:05:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -46,280 +46,280 @@ const products = [
             },
             {
                 id: "4",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.04 PM (2).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.04 PM (2).jpeg',
                 title: 'Rechargeable PRO LED 800 Light with 2.1 meter tripod stand',
                 price: 44000,
                 category: 'Electronics'
             },
             {
                 id: "5",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.04 PM.jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.04 PM.jpeg',
                 title: '12 inches LCD writing tab',
                 price: 7000,
                 category: 'Electronics'
             },
             {
                 id: "6",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.05 PM (1).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.05 PM (1).jpeg',
                 title: 'Men gift set with power bank',
                 price: 22000,
                 category: 'Accessories'
             },
             {
                 id: "7",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.05 PM (2).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.05 PM (2).jpeg',
                 title: 'Men gift set',
                 price: 22000,
                 category: 'Accessories'
             },
             {
                 id: "8",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.05 PM.jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.05 PM.jpeg',
                 title: 'Rechargeable PRO LED 600 Light with 2.1 meter tripod stand',
                 price: 39000,
                 category: 'bathroom'
             },
             {
                 id: "9",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.03 PM (1).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.03 PM (1).jpeg',
                 title: 'Hand bag',
                 price: 12000,
                 category: 'Accessories'
             },
             {
                 id: "10",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.34.53 PM.jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.34.53 PM.jpeg',
                 title: '2 in 1 Bath towel set super soft highly absorbent',
                 price: 5500,
                 category: 'Clothing & Fashion'
             },
             {
                 id: "12",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.34.53 PM (1).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.34.53 PM (1).jpeg',
                 title: 'Clothing racks',
                 price: 17000,
                 category: 'Home & Furniture'
             },
                   {
                 id: "13",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.34.52 PM.jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.34.52 PM.jpeg',
                 title: 'Fluffy fur cap',
                 price: 6500,
                 category: 'Clothing & Fashion'
             },
                 {
                 id: "14",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.34.51 PM.jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.34.51 PM.jpeg',
                 title: 'Fluffy fur cap',
                 price: 6500,
                 category: 'Clothing & Fashion'
             },
             {
                 id: "15",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.06 PM (1).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.06 PM (1).jpeg',
                 title: 'Juliana watch set',
                 price: 9999,
                 category: 'Accessories'
             }, 
             {
                 id: "16",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.06 PM (2).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.06 PM (2).jpeg',
                 title: 'Eternal love couples watch and bracelet set',
                 price: 23000,
                 category: 'Accessories'
             },
             {
                 id: "17",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.06 PM (3).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.06 PM (3).jpeg',
                 title: '5pcs Blue eye watch set',
                 price: 9500,
                 category: 'Accessories'
             },
              {
                 id: "18",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.07 PM (1).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.07 PM (1).jpeg',
                 title: '3 in 1 UV Led Manicure and Pedicure Nail Lamp',
                 price: 23000,
                 category: 'Beauty & Personal Care'
             },
                  {
                 id: "19",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.07 PM (2).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.07 PM (2).jpeg',
                 title: '3 in 1 UV Led Manicure and Pedicure Nail Lamp',
                 price: 23000,
                 category: 'Beauty & Personal Care'
             },
                  {
                 id: "18",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.07 PM.jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.07 PM.jpeg',
                 title: 'Knight and princess couples watch and bracelet set',
                 price: 23000,
                 category: 'Accessories'
             },
              {
                 id: "19",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.08 PM (1).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.08 PM (1).jpeg',
                 title: 'HQNova Professional Curling Iron',
                 price: 12000,
                 category: 'Beauty & Personal Care'
             },
              {
                 id: "20",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.08 PM (2).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.08 PM (2).jpeg',
                 title: 'Twin Swan Contrast home decor',
                 price: 31000,
                 category: 'Home Decor'
             },
               {
                 id: "21",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.08 PM.jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.08 PM.jpeg',
                 title: 'Mini True Wireless Stero earbuds',
                 price: 8500,
                 category: 'Electronic'
             },
               {
                 id: "22",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.09 PM (1).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.09 PM (1).jpeg',
                 title: 'Closed faux book',
                 price: 5000,
                 category: 'Home Decor'
             },
               {
                 id: "23",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.09 PM.jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.09 PM.jpeg',
                 title: 'Portable mini BT printer',
                 price: 17000,
                 category: 'Electronic'
             },
               {
                 id: "24",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.10 PM (1).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.10 PM (1).jpeg',
                  title: 'Rechargeable Multifunctional Astronaut Projector with Bluetooth Speaker/Fm Radio',
                 price: 27000,
                 category: 'Home Decor'
             },
             {
                 id: "25",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.10 PM (2).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.10 PM (2).jpeg',
                 title: 'Rechargeable Multifunctional Astronaut Projector with Bluetooth Speaker/Fm Radio',
                 price: 27000,
                 category: 'Home Decor'
             },
             {
                 id: "26",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.11 PM (1).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.11 PM (1).jpeg',
                 title: 'Soft Fluffy Home Rug For Living Room And Bedroom (160 by 230)',
                 price: 37000,
                 category: 'Home Decor'
             },
             {
                 id: "27",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.11 PM (2).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.11 PM (2).jpeg',
                 title: 'Soft Fluffy Home Rug For Living Room And Bedroom (160 by 230)',
                 price: 37000,
                 category: 'Home Decor'
             },
             {
                 id: "28",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.11 PM (3).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.11 PM (3).jpeg',
                 title: 'Soft Fluffy Home Rug For Living Room And Bedroom (160 by 230)',
                 price: 37000,
                 category: 'Home Decor'
             },
             {
                 id: "29",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.11 PM.jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.11 PM.jpeg',
                 title: 'Soft Fluffy Home Rug For Living Room And Bedroom (160 by 230)',
                 price: 37000,
                 category: 'Home Decor'
             },
             {
                 id: "30",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.12 PM (1).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.12 PM (1).jpeg',
                 title: 'Soft Fluffy Home Rug For Living Room And Bedroom (160 by 230)',
                 price: 37000,
                 category: 'Home Decor'
             },
                   {
                 id: "31",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.12 PM (2).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.12 PM (2).jpeg',
                 title: 'Astronaut Starry Bluetooth Projector',
                 price: 24000,
                 category: 'Electronic'
             },
             {
                 id: "32",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.12 PM.jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.12 PM.jpeg',
                 title: 'Soft Fluffy Home Rug For Living Room And Bedroom (160 by 230)',
                 price: 37000,
                 category: 'Home Decor'
             },
             {
                 id: "33",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.13 PM (1).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.13 PM (1).jpeg',
                 title: 'P47 wireless headphones',
                 price: 6500,
                 category: 'Electronic'
             },
             {
                 id: "34",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.13 PM (2).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.13 PM (2).jpeg',
                 title: 'STN-28 CAT HEADSET',
                 price: 9500,
                 category: 'Electronic'
             },
             {
                 id: "35",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.13 PM.jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.13 PM.jpeg',
                 title: '3 Partition jewelry organizer',
                 price: 7900,
                 category: 'Accessories'
             },
             {
                 id: "36",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.14 PM (1).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.14 PM (1).jpeg',
                 title: 'MF993BT',
                 price: 13000,
                 category: 'Electronic'
             },
             {
                 id: "33",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.14 PM (2).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.14 PM (2).jpeg',
                 title: 'Nodizz NPad 18 Adult',
                 price: 95000,
                 category: 'Electronic'
             },
             {
                 id: "33",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.14 PM (3).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.14 PM (3).jpeg',
                 title: 'Pink Jolie watch set',
                 price: 12000,
                 category: 'Accessories'
             },
             {
                 id: "33",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.14 PM.jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.14 PM.jpeg',
                 title: 'P9 PRO MAX HEADSET',
                 price: 9500,
                 category: 'Electronic'
             },
             {
                 id: "33",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.15 PM (1).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.15 PM (1).jpeg',
                 title: 'Amelia 3pcs watch',
                 price: 16000,
                 category: 'Accessories'
             },
             {
                 id: "33",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.15 PM (2).jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.15 PM (2).jpeg',
                 title: 'Astronaut Wall Clock',
                 price: 32000,
                 category: 'Home Decor'
             },
             {
                 id: "33",
-                image: 'img/WhatsApp Image 2026-03-16 at 9.35.15 PM.jpeg',
+                image: 'WhatsApp Image 2026-03-16 at 9.35.15 PM.jpeg',
                 title: 'Fola Jewelry Set',
                 price: 12900,
                 category: 'Accessories'
